5652755a80cb226671d012ec8065c2fb5f86b8a3
temppone <gtvtempone@gmail.com>
2021-07-25 21:58:37 -0300

    fix: historic component styles

--- src/components/Historic/index.tsx
+++ src/components/Historic/index.tsx
@@ -1,15 +1,96 @@
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import { HistoricContainer, HistoricIcon, HistoricItem, HistoricItemContent } from './styles';
+import { Undo, Redo, Sync } from '@material-ui/icons';
+import {
+    HistoricContainer,
+    HistoricIcon,
+    HistoricItem,
+    HistoricItemContent,
+    HistoricDate,
+} from './styles';
 
-const Historic = () => {
-    <HistoricContainer>
-        <HistoricIcon>
-            <ArrowDownwardIcon />
-        </HistoricIcon>
-        <HistoricItem>
-            <HistoricItemContent>Compra de brita</HistoricItemContent>
-        </HistoricItem>
-    </HistoricContainer>;
-};
+const Historic = () => (
+    <>
+        <HistoricContainer>
+            <HistoricIcon>
+                <Redo />
+            </HistoricIcon>
+            <HistoricItem>
+                <HistoricItemContent>Compra de brita 25 britas</HistoricItemContent>
+            </HistoricItem>
+            <HistoricDate>25 mai 2021</HistoricDate>
+        </HistoricContainer>
+        <HistoricContainer>
+            <HistoricIcon>
+                <Undo />
+            </HistoricIcon>
+            <HistoricItem>
+                <HistoricItemContent>Compra de brita 25 britas</HistoricItemContent>
+            </HistoricItem>
+            <HistoricDate>25 mai 2021</HistoricDate>
+        </HistoricContainer>
+        <HistoricContainer>
+            <HistoricIcon>
+                <Undo />
+            </HistoricIcon>
+            <HistoricItem>
+                <HistoricItemContent>Compra de brita 25 britas</HistoricItemContent>
+            </HistoricItem>
+            <HistoricDate>25 mai 2021</HistoricDate>
+        </HistoricContainer>
+        <HistoricContainer>
+            <HistoricIcon>
+                <Sync />
+            </HistoricIcon>
+            <HistoricItem>
+                <HistoricItemContent>Compra de brita 25 britas</HistoricItemContent>
+            </HistoricItem>
+            <HistoricDate>25 mai 2021</HistoricDate>
+        </HistoricContainer>
+        <HistoricContainer>
+            <HistoricIcon>
+                <Sync />
+            </HistoricIcon>
+            <HistoricItem>
+                <HistoricItemContent>Compra de brita 25 britas</HistoricItemContent>
+            </HistoricItem>
+            <HistoricDate>25 mai 2021</HistoricDate>
+        </HistoricContainer>
+        <HistoricContainer>
+            <HistoricIcon>
+                <Undo />
+            </HistoricIcon>
+            <HistoricItem>
+                <HistoricItemContent>Compra de brita 25 britas</HistoricItemContent>
+            </HistoricItem>
+            <HistoricDate>25 mai 2021</HistoricDate>
+        </HistoricContainer>
+        <HistoricContainer>
+            <HistoricIcon>
+                <Undo />
+            </HistoricIcon>
+            <HistoricItem>
+                <HistoricItemContent>Compra de brita 25 britas</HistoricItemContent>
+            </HistoricItem>
+            <HistoricDate>25 mai 2021</HistoricDate>
+        </HistoricContainer>
+        <HistoricContainer>
+            <HistoricIcon>
+                <Undo />
+            </HistoricIcon>
+            <HistoricItem>
+                <HistoricItemContent>Compra de brita 25 britas</HistoricItemContent>
+            </HistoricItem>
+            <HistoricDate>25 mai 2021</HistoricDate>
+        </HistoricContainer>
+        <HistoricContainer>
+            <HistoricIcon>
+                <Undo />
+            </HistoricIcon>
+            <HistoricItem>
+                <HistoricItemContent>Compra de brita 25 britas</HistoricItemContent>
+            </HistoricItem>
+            <HistoricDate>25 mai 2021</HistoricDate>
+        </HistoricContainer>
+    </>
+);
 
 export default Historic;
--- src/components/Historic/styles.ts
+++ src/components/Historic/styles.ts
@@ -1,17 +1,30 @@
 import styled from 'styled-components';
 
 export const HistoricContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
     width: 100%;
+    margin: 0.5rem 0;
+    border-bottom: 1px solid ${({ theme }) => theme.palette.secundaryDark};
+    padding: 1rem 0;
 `;
 
 export const HistoricItem = styled.div`
-    width: 100%;
+    padding: 1rem;
 `;
 
 export const HistoricIcon = styled.div`
-    width: 100%;
+    padding: 0.2rem;
 `;
 
 export const HistoricItemContent = styled.p`
+    font-size: 1rem;
+`;
+
+export const HistoricDate = styled.p`
     font-size: 1.2rem;
+    width: 10%;
+    text-align: center;
 `;
