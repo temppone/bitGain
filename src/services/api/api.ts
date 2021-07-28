957faad3d5576e44371888d2f8820e0186c55ad3
temppone <gtvtempone@gmail.com>
2021-07-27 21:40:40 -0300

    fix: api.ts

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,16 +1,24 @@
 /* eslint-disable @typescript-eslint/quotes */
 /* eslint-disable prettier/prettier */
+
+import { URLSearchParams } from 'url';
+
 /* eslint-disable operator-linebreak */
 export const apiBrita =
-    'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata//CotacaoDolarDia(dataCotacao=@';
+    'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata//CotacaoDolarDia(dataCotacao=@dataCotacao)';
 
-export const BRITA_VALUE_GET = (date: string) => ({
-    url: `${apiBrita}${date})`,
+export const BRITA_VALUE_GET = (date: string) => {
+    const teste = new URLSearchParams();
+    teste.append('datacotacao', date);
 
-    options: {
-        method: 'GET',
-        Headers: {
-            accept: 'application/json; odata.metadata=minimal',
+    return {
+        url: `${apiBrita}${teste.toString()}`,
+
+        options: {
+            method: 'GET',
+            Headers: {
+                accept: 'application/json; odata.metadata=minimal',
+            },
         },
-    },
-});
+    };
+};
